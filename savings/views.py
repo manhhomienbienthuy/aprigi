fdbfe4d599be9ce86dc80b3551c71d18f70cc8fb
Anh Tranngoc <manhhomienbienthuy@yahoo.com.vn>
2016-05-26 14:29:19 +0700

    Refactor and add test

--- savings/views.py
+++ savings/views.py
@@ -1,7 +1,7 @@
 from calendar import monthrange
 from datetime import timedelta
 
-from django.utils import timezone
+from django.utils import dateparse, timezone
 from django.views.generic.list import ListView
 
 from .models import Passbook
@@ -11,27 +11,34 @@ class PassbookView(ListView):
     model = Passbook
 
     def get_queryset(self):
-        objects_list = self.model.objects.all().order_by('start_date')
+        object_list = self.model.objects.all().order_by('start_date')
         is_open = self.request.GET.get('is_open')
         if is_open is not None:
-            objects_list = objects_list.filter(is_open__exact=is_open)
-        upcoming = self.request.GET.get('upcoming')
-        if upcoming:
-            if upcoming not in ('today', 'thisweek', 'thismonth'):
-                target_date = upcoming
-            else:
-                today = timezone.now()
-                if upcoming == 'today':
-                    target_date = today.date()
-                elif upcoming == 'thisweek':
-                    target_date = (today + timedelta(days=7)).date()
-                elif upcoming == 'thismonth':
-                    end_date = monthrange(today.year, today.month)[1]
-                    target_date = "{year}-{month}-{day}".format(
-                        year=today.year,
-                        month=today.month,
-                        day=end_date
-                    )
-            objects_list = objects_list.filter(is_open=True,
-                                               stop_date__lte=target_date)
-        return objects_list
+            object_list = object_list.filter(is_open__exact=is_open)
+        target_date = self._upcoming_date(self.request.GET.get('upcoming'))
+        if target_date:
+            object_list = object_list.filter(is_open=True,
+                                             stop_date__lte=target_date)
+        return object_list
+
+    def _upcoming_date(self, upcoming):
+        if not upcoming:
+            return None
+        if upcoming not in ('today', 'thisweek', 'thismonth'):
+            try:
+                return dateparse.parse_date(upcoming)
+            except ValueError:
+                return None
+        today = timezone.now()
+        if upcoming == 'thismonth':
+            end_date = monthrange(today.year, today.month)[1]
+            return "{year}-{month}-{day}".format(
+                year=today.year,
+                month=today.month,
+                day=end_date
+            )
+        if upcoming == 'thisweek':
+            delta = timedelta(days=7)
+        else:
+            delta = timedelta(0)
+        return (today + delta).date()
