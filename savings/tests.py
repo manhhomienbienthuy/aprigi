a48916bea571bbc4dd5979459d983402cbb9f47c
Anh Tranngoc <manhhomienbienthuy@yahoo.com.vn>
2016-06-01 16:42:03 +0700

    Savings: Add more tests

--- savings/tests.py
+++ savings/tests.py
@@ -1,8 +1,10 @@
 from calendar import monthrange
 from datetime import datetime, timedelta
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from .forms import PassbookForm
 from .models import Passbook
 
 
@@ -14,8 +16,8 @@ class PassbookTest(TestCase):
             number="001",
             account_number="001",
             amount=10000000,
-            period_type=2,
             period=1,
+            period_type=2,
             rate=5.0,
             start_date=self.today,
             stop_date=self.today + timedelta(days=31),
@@ -61,6 +63,7 @@ class PassbookViewTest(TestCase):
 
     def setUp(self):
         self.today = datetime.today()
+        self.url = reverse('savings:passbook_list')
         self.closed_passbook = Passbook.objects.create(
             number="001",
             account_number="001",
@@ -140,14 +143,14 @@ class PassbookViewTest(TestCase):
         response = self.client.get('/savings/')
         self.assertRedirects(response, '/en/savings/', target_status_code=302)
         response = self.client.get('/savings/passbook/')
-        self.assertRedirects(response, '/en/savings/passbook/')
+        self.assertRedirects(response, self.url)
 
     def test_index_view(self):
         response = self.client.get('/en/savings/')
-        self.assertRedirects(response, '/en/savings/passbook/')
+        self.assertRedirects(response, self.url)
 
     def test_passbook_view(self):
-        response = self.client.get('/en/savings/passbook/')
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertSequenceEqual(
             response.context['object_list'],
@@ -162,7 +165,7 @@ class PassbookViewTest(TestCase):
         )
 
     def test_passbook_view_filter_open(self):
-        response = self.client.get('/en/savings/passbook/?is_open=1')
+        response = self.client.get(self.url + '?is_open=1')
         self.assertEqual(response.status_code, 200)
         self.assertSequenceEqual(
             response.context['object_list'],
@@ -176,7 +179,7 @@ class PassbookViewTest(TestCase):
         )
 
     def test_passbook_view_filter_close(self):
-        response = self.client.get('/en/savings/passbook/?is_open=0')
+        response = self.client.get(self.url + '?is_open=0')
         self.assertEqual(response.status_code, 200)
         self.assertSequenceEqual(
             response.context['object_list'],
@@ -186,7 +189,7 @@ class PassbookViewTest(TestCase):
         )
 
     def test_passbook_view_filter_today(self):
-        response = self.client.get('/en/savings/passbook/?upcoming=' +
+        response = self.client.get(self.url + '?upcoming=' +
                                    self.today.strftime('%Y-%m-%d'))
         expected_object_list = [self.today_passbook]
         if self.today.month < (self.today + timedelta(days=1)).month:
@@ -196,7 +199,7 @@ class PassbookViewTest(TestCase):
             response.context['object_list'], expected_object_list)
 
     def test_passbook_view_filter_thisweek(self):
-        response = self.client.get('/en/savings/passbook/?upcoming=thisweek')
+        response = self.client.get(self.url + '?upcoming=thisweek')
         expected_object_list = [
             self.today_passbook,
             self.this_week_passbook,
@@ -208,7 +211,7 @@ class PassbookViewTest(TestCase):
             response.context['object_list'], expected_object_list)
 
     def test_passbook_view_filter_thismonth(self):
-        response = self.client.get('/en/savings/passbook/?upcoming=thismonth')
+        response = self.client.get(self.url + '?upcoming=thismonth')
         expected_object_list = [self.today_passbook]
         if self.today.month == (self.today + timedelta(days=1)).month:
             expected_object_list.append(self.this_week_passbook)
@@ -219,7 +222,7 @@ class PassbookViewTest(TestCase):
 
     def test_passbook_view_filter_by_date(self):
         response = self.client.get(
-            "/en/savings/passbook/?upcoming=%s-12-31" % (self.today.year + 2))
+            self.url + "?upcoming=%s-12-31" % (self.today.year + 2))
         self.assertEqual(response.status_code, 200)
         self.assertSequenceEqual(
             response.context['object_list'],
@@ -233,7 +236,7 @@ class PassbookViewTest(TestCase):
         )
 
     def test_passbook_view_filter_by_invalid_date(self):
-        response = self.client.get('/en/savings/passbook/?upcoming=2016-31-12')
+        response = self.client.get(self.url + '?upcoming=2016-31-12')
         self.assertEqual(response.status_code, 200)
         self.assertSequenceEqual(
             response.context['object_list'],
@@ -246,3 +249,35 @@ class PassbookViewTest(TestCase):
                 self.next_year_passbook,
             ]
         )
+
+
+class PassbookFormTest(TestCase):
+
+    def setUp(self):
+        self.valid_data = {
+            'number': '001',
+            'account_number': '001',
+            'amount': 10,
+            'period_type': 2,
+            'period': 1,
+            'rate': 5.0,
+            'start_date': '2016-06-01',
+            'stop_date': '2016-07-01',
+            'is_open': True,
+            'notes': 'Note'
+        }
+
+    def test_submit_success(self):
+        form = PassbookForm(self.valid_data)
+        self.assertTrue(form.is_valid)
+        instance = form.save()
+        self.assertEqual(instance.number, '001')
+        self.assertEqual(instance.account_number, '001')
+        self.assertEqual(instance.amount, 10)
+        self.assertEqual(instance.period_type, 2)
+        self.assertEqual(instance.period, 1)
+        self.assertEqual(instance.rate, 5.0)
+        self.assertEqual(instance.start_date, datetime(2016, 6, 1).date())
+        self.assertEqual(instance.stop_date, datetime(2016, 7, 1).date())
+        self.assertEqual(instance.is_open, True)
+        self.assertEqual(instance.notes, 'Note')
