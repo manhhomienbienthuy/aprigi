8dd07ff21bfbbef48435679f4ed576d3da2934f6
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2016-06-01 18:01:35 +0700

    Savings: Add passbook update view

--- savings/urls.py
+++ savings/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
 
-from .views import PassbookCreateView, PassbookView
+from .views import PassbookCreateView, PassbookUpdateView, PassbookView
 
 app_name = 'savings'
 
@@ -12,4 +12,6 @@ urlpatterns = [
     url(r'^passbook/$', PassbookView.as_view(), name='passbook_list'),
     url(r'^passbook/create$', PassbookCreateView.as_view(),
         name='passbook_create'),
+    url(r'^passbook/(?P<pk>\d+)/update$', PassbookUpdateView.as_view(),
+        name='passbook_update')
 ]
