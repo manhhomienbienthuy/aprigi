87fbeb00c289291ce7c780ac975c2f89ae877717
Anh Tranngoc <manhhomienbienthuy@yahoo.com.vn>
2016-06-03 07:58:25 +0700

    Fix coding style

--- savings/admin.py
+++ savings/admin.py
@@ -5,8 +5,8 @@ from .models import Passbook
 
 class PassbookAdmin(admin.ModelAdmin):
     list_display = ('number', 'account_number', 'amount', 'period', 'rate',
-                    'start_date', 'stop_date', 'is_open', 'interest')
-    list_filter = ['is_open', 'period', 'start_date', 'stop_date']
+                    'start_date', 'stop_date', 'is_open', 'interest', )
+    list_filter = ['is_open', 'period', 'start_date', 'stop_date', ]
 
 
 admin.site.register(Passbook, PassbookAdmin)
