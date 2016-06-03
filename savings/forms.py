8bd52f0c9287bc62151207d3873e9c9082427855
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2016-06-03 13:03:12 +0700

    Savings: use form class for filter

--- savings/forms.py
+++ savings/forms.py
@@ -1,10 +1,15 @@
-from django.forms import ModelForm
+from django import forms
 
 from .models import Passbook
 
 
-class PassbookForm(ModelForm):
+class PassbookForm(forms.ModelForm):
 
     class Meta:
         model = Passbook
         fields = '__all__'
+
+
+class PassbookSearchForm(forms.Form):
+    is_open = forms.NullBooleanField()
+    upcoming = forms.DateField(required=False)
--- savings/views.py
+++ savings/views.py
@@ -2,10 +2,10 @@ from calendar import monthrange
 from datetime import timedelta
 
 from django.core.urlresolvers import reverse
-from django.utils import dateparse, timezone
+from django.utils import timezone
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
-from .forms import PassbookForm
+from .forms import PassbookForm, PassbookSearchForm
 from .models import Passbook
 
 
@@ -14,32 +14,38 @@ class PassbookView(ListView):
 
     def get_queryset(self):
         object_list = self.model.objects.all().order_by('start_date')
-        is_open = self.request.GET.get('is_open')
-        if is_open is not None:
-            object_list = object_list.filter(is_open__exact=is_open)
-        target_date = self._upcoming_date(self.request.GET.get('upcoming'))
-        if target_date:
-            object_list = object_list.filter(is_open=True,
-                                             stop_date__lte=target_date)
+        form = PassbookSearchForm(self.request.GET or None)
+        if form.is_valid():
+            is_open = form.cleaned_data.get('is_open')
+            if is_open is not None:
+                object_list = object_list.filter(is_open=is_open)
+            target_date = form.cleaned_data.get('upcoming')
+            if target_date:
+                object_list = object_list.filter(is_open=True,
+                                                 stop_date__lte=target_date)
         return object_list
 
-    def _upcoming_date(self, upcoming):
-        if not upcoming:
-            return None
-        if upcoming not in ('thisweek', 'thismonth'):
-            try:
-                return dateparse.parse_date(upcoming)
-            except ValueError:
-                return None
+    def get_context_data(self):
+        form = PassbookSearchForm()
+        context = super().get_context_data()
+        context.update(self._upcoming_date())
+        context.update({
+            'form': form,
+        })
+        return context
+
+    def _upcoming_date(self):
         today = timezone.now()
-        if upcoming == 'thismonth':
-            end_date = monthrange(today.year, today.month)[1]
-            return "{year}-{month}-{day}".format(
-                year=today.year,
-                month=today.month,
-                day=end_date
-            )
-        return (today + timedelta(days=7)).date()
+        end_date_of_month = monthrange(today.year, today.month)[1]
+        end_of_month = "{year}-{month}-{day}".format(
+            year=today.year,
+            month=today.month,
+            day=end_date_of_month,
+        )
+        return {
+            'thisweek': (today + timedelta(days=7)).strftime('%Y-%m-%d'),
+            'thismonth': end_of_month
+        }
 
 
 class PassbookCreateView(CreateView):
