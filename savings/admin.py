1262e1995be719bb80377182fc815f4deeb46b61
Anh Tranngoc <manhhomienbienthuy@yahoo.com.vn>
2016-06-06 09:07:07 +0700

    Savings: Add Withdraw model

--- savings/admin.py
+++ savings/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Passbook
+from .models import Passbook, Withdraw
 
 
 class PassbookAdmin(admin.ModelAdmin):
@@ -9,4 +9,10 @@ class PassbookAdmin(admin.ModelAdmin):
     list_filter = ['is_open', 'period', 'start_date', 'stop_date', ]
 
 
+class WithdrawAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'is_open', 'date', )
+    list_filter = ['is_open', ]
+
+
 admin.site.register(Passbook, PassbookAdmin)
+admin.site.register(Withdraw, WithdrawAdmin)
