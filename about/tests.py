e6cd976837ff6d097b364a9e8ee1d93313be7c10
Anh Tranngoc <manhhomienbienthuy@yahoo.com.vn>
2016-06-13 11:47:34 +0700

    About: add tests

--- about/tests.py
+++ about/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+class AboutTest(TestCase):
+    def assertView(self, name):
+        self.assertContains(self.client.get(reverse('about:%s' %name)), 'Aprigi')
+
+    def test_about_this_site(self):
+        self.assertView('this_site')
+
+    def test_about_us(self):
+        self.assertView('us')
+
+    def test_about_privacy(self):
+        self.assertView('us')
+
+    def test_about_terms(self):
+        self.assertView('terms')
+
+    def test_about_contact(self):
+        self.assertView('contact')
