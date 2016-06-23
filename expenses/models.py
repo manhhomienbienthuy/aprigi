c19f53f11c1a6c946837e5b6b477c697be6e4911
Anh Tranngoc <manhhomienbienthuy@yahoo.com.vn>
2016-06-23 10:47:25 +0700

    Expense: add models

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Income(models.Model):
+    amount = models.IntegerField(_('amount'))
+    date = models.DateField(_('income on'), default=timezone.now)
+
+    class Meta:
+        verbose_name = _('Income')
+        verbose_name_plural = _('Incomes')
+
+    def __str__(self):
+        return "income ${self.amount} on {self.date}".format(self=self)
+
+
+class Expense(models.Model):
+    amount = models.IntegerField(_('amount'))
+    date = models.DateField(_('expense on'),
+                            default=timezone.now, db_index=True)
+    purpose = models.CharField(_('purpose'), max_length=100)
+
+    class Meta:
+        verbose_name = _('Expense')
+        verbose_name_plural = _('Expenses')
+
+    def __str__(self):
+        return "expended ${self.amount} on {self.date}".format(self=self)
