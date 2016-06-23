7aef3757cce6a4af20f393e363b5d59e8a091a25
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2016-06-23 10:47:25 +0700

    Expense: add models

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Expense, Income
+
+
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'amount', 'date', )
+    list_filter = ['amount', 'date', ]
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'amount', 'date', 'purpose', )
+    list_filter = ['amount', 'date', ]
+
+
+admin.site.register(Income, IncomeAdmin)
+admin.site.register(Expense, ExpenseAdmin)
