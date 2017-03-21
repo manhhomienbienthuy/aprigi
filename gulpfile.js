40cb05f3865416ad944934651e3b435708811303
Anh Tranngoc <manhhomienbienthuy@yahoo.com.vn>
2017-03-21 10:52:36 +0700

    Update plugins and prepare for React

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,37 @@
 'use strict';
 
 // Load basic plugins
-var gulp        = require('gulp'),
-    gulpif      = require('gulp-if'),
-    argv        = require('yargs').argv,
-    production  = !!argv.production; // --production
+const gulp        = require('gulp'),
+      gulpif      = require('gulp-if'),
+      argv        = require('yargs').argv,
+      production  = !!argv.production; // --production
 
 // Load plugins for stylesheet task
-var sass          = require('gulp-sass'),
-    autoprefixer  = require('gulp-autoprefixer'),
-    csslint       = require('gulp-csslint'),
-    shorthand     = require('gulp-shorthand'),
-    combine       = require('gulp-combine-mq'),
-    cssmin        = require('gulp-clean-css'),
-    sourcemaps    = require('gulp-sourcemaps'),
-    plumber       = require('gulp-plumber');
+const sass          = require('gulp-sass'),
+      autoprefixer  = require('gulp-autoprefixer'),
+      csslint       = require('gulp-csslint'),
+      shorthand     = require('gulp-shorthand'),
+      combine       = require('gulp-combine-mq'),
+      cssmin        = require('gulp-clean-css'),
+      sourcemaps    = require('gulp-sourcemaps'),
+      plumber       = require('gulp-plumber');
 
 // Load plugins for javascript task
-var eslint  = require('gulp-eslint'),
-    babel   = require('gulp-babel'),
-    es2015  = require('babel-preset-es2015'),
-    uglify  = require('gulp-uglify');
+const eslint  = require('gulp-eslint'),
+      babel   = require('gulp-babel'),
+      uglify  = require('gulp-uglify');
 
-var config = {
+// Load plugins for React
+const browserify = require('browserify'),
+      babelify   = require('babelify'),
+      source     = require('vinyl-source-stream'),
+      buffer     = require('vinyl-buffer'),
+      glob       = require('glob'),
+      transform  = require('vinyl-transform');
+
+const config = {
     src: {
-        base: '../static_src/',
+        base: 'static_src/',
         init: function() {
             this.js         = this.base + 'js/**/*.js';
             this.js_copy    = this.base + 'js/lib/*.js';
@@ -32,24 +39,39 @@ var config = {
             this.css        = this.base + 'scss/*.scss';
             this.css_main   = this.base + 'scss/vpyeu.scss';
             this.img        = this.base + 'img/*.{jpg,jpeg,png,gif,ico,svg}';
+            this.react      = this.base + 'flux/**/*.js';
             return this;
         }
     }.init(),
     dest: {
-        base: '../static/',
+        base: 'static/',
         init: function() {
             this.js       = this.base + 'js';
             this.js_copy  = this.base + 'js/lib';
             this.css      = this.base + 'css';
             this.img      = this.base + 'img';
+            this.react    = this.base + 'js'
             return this;
         }
     }.init(),
-    autoprefixer: {browsers: ['last 2 versions', '> 1%', 'iOS 7']},
-    babel: {presets: [es2015]},
-    uglify: {preserveComments: 'license'},
+    autoprefixer: {
+        browsers: [
+            'last 2 versions',
+            '> 1%',
+            'iOS 7'
+        ]
+    },
+    babel: {
+        presets: [
+            'es2015',
+            'react'
+        ]
+    },
+    uglify: {
+        preserveComments: 'license'
+    },
     plumber: {
-        errorHandler: function (error) {
+        errorHandler: (error) => {
             console.log(error.toString());
             this.emit('end');
         }
@@ -72,6 +94,24 @@ gulp.task('stylesheet', () => {
         .pipe(gulp.dest(config.dest.css));
 });
 
+gulp.task('react-lint', () => {
+    return gulp
+        .src(config.src.react)
+        .pipe(eslint('.eslintrc.json'))
+        .pipe(eslint.format());
+});
+
+gulp.task('javascript-react', ['react-lint'], () => {
+    const src = glob.sync(config.src.react)
+    return browserify({entries: src})
+        .transform(babelify.configure(config.babel))
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(gulpif(production, uglify()))
+        .pipe(gulp.dest(config.dest.react))
+});
+
 gulp.task('javascript-compile', () => {
     return gulp
         .src(config.src.js_compile)
@@ -89,7 +129,11 @@ gulp.task('javascript-copy', () => {
         .pipe(gulp.dest(config.dest.js_copy));
 });
 
-gulp.task('javascript', ['javascript-compile', 'javascript-copy']);
+gulp.task('javascript', [
+    'javascript-react',
+    'javascript-compile',
+    'javascript-copy'
+]);
 
 gulp.task('images', () => {
     // At this time, it simply copy all images file
@@ -100,6 +144,7 @@ gulp.task('images', () => {
 
 gulp.task('watch', ['stylesheet', 'javascript', 'images'], () => {
     gulp.watch(config.src.css, ['stylesheet']);
+    gulp.watch(config.src.react, ['javascript-react']);
     gulp.watch(config.src.js, ['javascript']);
     gulp.watch(config.src.img, ['images']);
 });
