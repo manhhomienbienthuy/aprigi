1da6f2ed26254f67817d5b96c399167cccaf8cd0
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2017-03-21 12:52:53 +0700

    Update docker and Makefile

--- aprigi/settings.py
+++ aprigi/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'aprigi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DBNAME', ''),
+        'NAME': os.environ.get('POSTGRES_DB', ''),
         'USER': os.environ.get('POSTGRES_USER', ''),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
         'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),
