8d62c0765befe453aa0bcb80c199e09f1fe89e32
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2017-03-22 08:18:54 +0700

    Fix gulp uglify

--- static_src/js/vpyeu.js
+++ static_src/js/vpyeu.js
@@ -1,7 +1,7 @@
 /*!
  * Script for Aprigi
  * Description: The app for my April girl
- * Copyright (C) 2016 Anh Tranngoc
+ * Copyright (C) 2016-present Anh Tranngoc
  * This file is distributed under the same license as the aprigi package.
  * Anh Tranngoc <naa@sfc.wide.ad.jp>, 2016.
  */
