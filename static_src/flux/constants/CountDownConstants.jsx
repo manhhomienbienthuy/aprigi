c9145b3aa756dbe7aeb92a49fee017cde884e0c2
Anh Tranngoc <manhhomienbienthuy@yahoo.com.vn>
2017-03-29 09:25:09 +0700

    Add CountDown to React

---
+++ static_src/flux/constants/CountDownConstants.jsx
@@ -0,0 +1,12 @@
+/**
+ * --------------------------------------------------------------------------
+ * Aprigi: CountDownConstants
+ * This file is distributed under the same license as the aprigi package.
+ * --------------------------------------------------------------------------
+ */
+
+'use strict';
+
+export const CountDownConstants = {
+    CHANGE_EVENT: 'countDownChangeEvent'
+};
